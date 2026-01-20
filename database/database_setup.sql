647eee537a1feb50c58e0b46faa550781496644f
lilianeuwase2 <l.uwase@alustudent.com>
2026-01-20 22:32:41 +0200

    created the transaction table with the corresponding attributes of transaction id, amount, transaction date etc

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -4,3 +4,14 @@ CREATE TABLE users (
     phone_number VARCHAR(15) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE transaction (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_date DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    
+);  
