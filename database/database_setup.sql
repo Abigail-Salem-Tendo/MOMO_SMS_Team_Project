ae61135380346891f1fc570382763f65f0142af1
Kiguru-Alu <b.mahui@alustudent.com>
2026-01-28 23:06:28 +0200

    Fixing more bugs

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -77,53 +77,6 @@ CREATE TABLE IF NOT EXISTS system_logs (
 
 
 
--- 4. INSERT CATEGORIES (Must be first)
-INSERT INTO transaction_categories (category_id, category_name) VALUES 
-(1, 'P2P_RECEIVE'), (2, 'P2P_SEND'), (3, 'MERCHANT_PAY'), (4, 'WITHDRAWAL'), (5, 'BILL_PAY');
-
--- 5. INSERT USERS (Must be second)
-INSERT INTO users (user_id, name, phone_number, entity_type) VALUES 
-(1, 'My Device', '0788000000', 'SELF'),
-(2, 'Jane Smith', '0788123013', 'INDIVIDUAL'),
-(3, 'Samuel Carter', '250791666666', 'INDIVIDUAL'),
-(4, 'Agent Sophia', '250790777777', 'AGENT'),
-(5, 'Airtime', 'MTN_AIRTIME', 'MERCHANT'),
-(6, 'Robert Brown', '0788999999', 'INDIVIDUAL');
-
-
-INSERT INTO user_transactions (transaction_id, user_id, role, balance_after) VALUES
-(1, 2, 'SENDER', NULL),
-(1, 1, 'RECEIVER', 2000.00),
-(2, 1, 'SENDER', 400.00),
-(2, 3, 'RECEIVER', NULL),
-(3, 1, 'SENDER', 6400.00),
-(3, 4, 'RECEIVER', NULL),
-(4, 1, 'SENDER', 25280.00),
-(4, 5, 'RECEIVER', NULL),
-(5, 1, 'SENDER', 9880.00),
-(5, 6, 'RECEIVER', NULL);
-
--- 6. INSERT TRANSACTIONS (Must be third - The Parents)
-INSERT INTO transactions (transaction_id, external_ref_id, transaction_date, amount, fees, category_id, raw_message) VALUES
-(1, '76662021700', '2024-05-10 16:30:51', 2000.00, 0.00, 1, 'Imported from XML: Received 2000'),
-(2, '51732411227', '2024-05-10 21:32:32', 600.00, 0.00, 2, 'Imported from XML: Payment to Samuel'),
-(3, '14098463509', '2024-05-26 02:10:27', 20000.00, 350.00, 4, 'Imported from XML: Agent Withdrawal'),
-(4, '13913173274', '2024-05-12 11:41:28', 2000.00, 0.00, 5, 'Imported from XML: Airtime'),
-(5, '26614842768', '2024-05-12 17:58:15', 1000.00, 0.00, 2, 'Imported from XML: Payment to Robert');
-
--- 7. INSERT LOGS (Must be last - The Children)
-INSERT INTO system_logs (transaction_id, log_level, status, message) VALUES
-(1, 'INFO', 'COMPLETED', 'Parsed incoming P2P from XML batch.'),
-(2, 'INFO', 'COMPLETED', 'Verified outgoing payment to Samuel.'),
-(3, 'INFO', 'COMPLETED', 'Agent withdrawal verified successfully.'),
-(4, 'INFO', 'COMPLETED', 'Airtime token generated.'),
-(5, 'INFO', 'COMPLETED', 'Payment to Robert confirmed.');
-
-
-
-
--- 8. VERIFY
-SELECT * FROM transactions;
 
 
 
