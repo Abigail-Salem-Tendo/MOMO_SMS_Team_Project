554706b85c8968f67cbd0f717918254ff8d6d185
Abigail Salem Tendo <salemabigail10@gmail.com>
2026-02-02 09:42:11 +0200

    testing the linear function with the parsed xml data

--- dsa/dsa_comparison.py
+++ dsa/dsa_comparison.py
@@ -3,6 +3,7 @@ This is a script that will compare the different times of
 linear search and dictionary search
 """
 import time
+from xml_parsing import parse_sms_xml
 
 #the sample data set goes here;
 
@@ -13,3 +14,19 @@ def linear_search(transaction_list, target_id):
         if transaction["id"] == target_id:
             return transaction
     return None
+
+#testing
+if __name__ == "__main__":
+    data = parse_sms_xml("modified_sms_v2.xml")
+
+    #find and id
+    if data:
+        test_id = data[0]["id"]
+        print(f"search for : {test_id}")
+        start = time.perf_counter()
+        result = linear_search(data, test_id)
+        end = time.perf_counter()
+
+        if result:
+            print(f"Found Transaction! Body: {result['body'][:50]}...")
+            print(f"Time taken: {end - start:.8f}s")
--- dsa/xml_parsing.py
+++ dsa/xml_parsing.py
@@ -9,21 +9,30 @@ import xml.etree.ElementTree as ET
 def parse_sms_xml(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
-
     transactions = []
 
     for sms in root.findall("sms"):
         transaction = {
-            "id": int(sms.find("id").text),
-            "type": sms.find("type").text,
-            "amount": int(sms.find("amount").text),
-            "sender": sms.find("sender").text,
-            "receiver": sms.find("receiver").text,
-            "timestamp": sms.find("timestamp").text
+            "id": sms.get("date"),
+            "sender": sms.get("address"),
+            "body": sms.get("body"),
+            "readable_date": sms.get("readable_date")
         }
         transactions.append(transaction)
-
     return transactions
+    #
+    # for sms in root.findall("sms"):
+    #     transaction = {
+    #         "id": int(sms.find("id").text),
+    #         "type": sms.find("type").text,
+    #         "amount": int(sms.find("amount").text),
+    #         "sender": sms.find("sender").text,
+    #         "receiver": sms.find("receiver").text,
+    #         "timestamp": sms.find("timestamp").text
+    #     }
+    #     transactions.append(transaction)
+    #
+    # return transactions
 
 if __name__ == "__main__":
     data = parse_sms_xml("modified_sms_v2.xml")
