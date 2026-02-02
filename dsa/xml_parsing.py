22785419cb4faeb0faa71551971167498867c8fc
lilianeuwase2 <l.uwase@alustudent.com>
2026-02-02 08:46:45 +0200

    created a function in  xml_parsing.py that reads xml file , loops through each tag

--- dsa/xml_parsing.py
+++ dsa/xml_parsing.py
@@ -3,3 +3,35 @@ parsing the xml file in python
 convert sms records from xml to list of dictionaries
 """
 
+import xml.etree.ElementTree as ET
+
+
+def parse_sms_xml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    transactions = []
+
+    for sms in root.findall("sms"):
+        transaction = {
+            "id": int(sms.find("id").text),
+            "type": sms.find("type").text,
+            "amount": int(sms.find("amount").text),
+            "sender": sms.find("sender").text,
+            "receiver": sms.find("receiver").text,
+            "timestamp": sms.find("timestamp").text
+        }
+        transactions.append(transaction)
+
+    return transactions
+
+if __name__ == "__main__":
+    data = parse_sms_xml("modified_sms_v2.xml")
+    for record in data:
+        print(record)
+        
+
+
+
+
+
