59561881c2e72494caf954d0eed38cbaa8a4d9dd
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-02 11:04:37 +0200

    GET transaction by {id} endpoint

--- backend/app.py
+++ backend/app.py
@@ -50,3 +50,37 @@ def get_all_transactions():
     finally:
         connection.close()
 
+
+# GET a specific transaction by using its transaction_ID and get all info related to that transaction
+@app.route('/transactions/<int:transaction_id>', methods=['GET'])
+def get_transaction_by_id(transaction_id):
+    try:
+        connection = get_db_connection()
+    except pymysql.MySQLError as e:
+        abort(500, description=f"Database connection failed: {e}")
+    try:
+        with connection.cursor() as cursor:
+            sql = """
+                select 
+                    t.transaction_id, t.external_ref_id, t.transaction_date,
+                    tc.category_name as transaction_category,
+                    u_sender.name as sender, u_sender.entity_type as sender_entity_type,
+                    t.amount,
+                    u_receiver.name as receiver, u_receiver.entity_type as receiver_entity_type,
+                    t.status, t.raw_message
+                from transactions t
+                join transaction_categories tc on t.category_id = tc.category_id
+                join user_transactions ut_s on t.transaction_id = ut_s.transaction_id and ut_s.role = 'SENDER'
+                join users u_sender on ut_s.user_id = u_sender.user_id
+                join user_transactions ut_r on t.transaction_id = ut_r.transaction_id and ut_r.role = 'RECEIVER'
+                join users u_receiver on ut_r.user_id = u_receiver.user_id
+                where t.transaction_id = %s;
+            """
+            cursor.execute(sql, (transaction_id,))
+            result = cursor.fetchone()
+        if result:
+            return jsonify(result), 200
+        else:
+            abort(404, description="Transaction not found")
+    finally:
+        connection.close()
