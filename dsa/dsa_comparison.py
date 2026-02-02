49d043b0f410f297dae88e935d87523b88817043
Abigail Salem Tendo <salemabigail10@gmail.com>
2026-02-02 08:03:22 +0200

    Add linear search function to look for a transaction using its id

--- dsa/dsa_comparison.py
+++ dsa/dsa_comparison.py
@@ -2,3 +2,14 @@
 This is a script that will compare the different times of
 linear search and dictionary search
 """
+import time
+
+#the sample data set goes here;
+
+#Linear search method
+def linear_search(transaction_list, target_id):
+    """This function searches for a transaction using its id"""
+    for transaction in transaction_list:
+        if transaction["id"] == target_id:
+            return transaction
+    return None
